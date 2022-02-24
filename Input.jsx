45a77c0642135bc79da3f80e09befc14227214f8
fadarash <fadarash@gmail.com>
2022-02-24 10:54:58 +0330

    "validation by utils"

---
+++ Input.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { validate } from '../utils/validators'
+
+const INPUT_STATES = {
+  UNTOUCHED: 'UNTOUCHED',
+  VALID: 'VALID',
+  INVALID: 'INVALID',
+}
+
+function Input({ label, id, type, validators, errorText }) {
+  const [inputState, setInputState] = useState(INPUT_STATES.UNTOUCHED)
+
+  const validateInput = (value) => {
+    if (!validate(value, validators)) {
+      setInputState(INPUT_STATES.INVALID)
+    } else {
+      setInputState(INPUT_STATES.VALID)
+    }
+  }
+
+  const handleChange = (e) => {
+    if (inputState !== INPUT_STATES.UNTOUCHED) {
+      const value = e.target.value
+      validateInput(value)
+    }
+  }
+
+  const handleBlur = (e) => {
+    validateInput(e.target.value)
+  }
+
+  return (
+    <div
+      className={
+        inputState === INPUT_STATES.INVALID
+          ? 'form-input form-input--invalid'
+          : 'form-input'
+      }
+      data-testid="form-input"
+    >
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type} onChange={handleChange} onBlur={handleBlur} />
+      <p>{inputState === INPUT_STATES.INVALID && errorText}</p>
+    </div>
+  )
+}
+
+export default Input
---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Input from './components/Input'
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_MAXLENGTH,
+  VALIDATOR_MIN,
+  VALIDATOR_MAX,
+  VALIDATOR_EMAIL,
+  VALIDATOR_PASSWORD
+} from './utils/validators'
+
+function App() {
+  return (
+    <div className="app">
+      <Input type="text" label="Name" id="name" 
+      validators={[VALIDATOR_REQUIRE()]} 
+      errorText="name must be valid"
+      />
+      <Input type="text" label="Username" id="username"
+        validators={[VALIDATOR_MINLENGTH(4), VALIDATOR_MAXLENGTH(12)]} 
+        errorText="username must be valid"
+      />
+      <Input type="text" label="Email" id="email"
+        validators={[VALIDATOR_EMAIL()]}
+        errorText="email must be valid"
+      />
+      <Input type="text" label="Age" id="age"
+        validators={[VALIDATOR_MIN(18), VALIDATOR_MAX(40)]}
+        errorText="age must be valid"
+      />
+      <Input type="password" label="Password" id="password"
+        validators={[VALIDATOR_PASSWORD()]}
+        errorText="Password is not acceptable"
+      />
+    </div>
+  )
+}
+export default App
